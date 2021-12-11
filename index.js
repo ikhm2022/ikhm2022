21fb67be923b93cbbdd6a05a471dfd517c667528
ikhm2022 <95961222+ikhm2022@users.noreply.github.com>
2021-12-11 22:53:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const download = require("download");
 const fs = require("fs");
 
 async function main() {
-  const books = ['33']
+  const books = ['33', '372']
   for (const bid of books) {
     await saveBookInfo(bid)
     await saveCapterContent(bid)
